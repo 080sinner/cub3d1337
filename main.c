1ba1bb29696fd12cdf17525572cdce3a071356f5
Florian Bindereif <fbindere@1-A-6.42heilbronn.de>
2022-03-14 21:47:05 +0100

    comp fixed

--- main.c
+++ main.c
@@ -3,14 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: eozben <eozben@student.42.fr>              +#+  +:+       +#+        */
+/*   By: fbindere <fbindere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/12 19:00:58 by eozben            #+#    #+#             */
-/*   Updated: 2022/03/13 18:07:45 by eozben           ###   ########.fr       */
+/*   Updated: 2022/03/14 21:46:07 by fbindere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+
+void	map_error(t_map *map, char *str, char *error_msg);
 
 int	ft_iswhitespace(char c)
 {
@@ -32,20 +35,6 @@ int	ft_is_empty_line(char *s)
 	return (0);
 }
 
-// int	ft_check_pattern_middle(char *str)
-// {
-// 	int	i;
-
-// 	i = 0;
-// 	while (ft_iswhitespace(str[i]))
-// 		i++;
-// 	if (str[i] != '1')
-// 		return (0);
-// 	if (str[ft_strlen(str) - 2] != '1')
-// 		return (0);
-// 	return (1);
-// }
-
 // int	check_valid_map(int fd, char *str)
 // {	
 // 	(void)fd;
@@ -66,14 +55,33 @@ int	skip_whitespaces(char *str)
 	return (i);
 }
 
-void	read_path(t_map *map, char *indentifier, char *str, int i)
+void	read_path(t_map *map, char indentifier, char *str, int i)
 {
-	i += 2;
-	i += skip_whitespaces(str[i]);
-	
-	map->no_path = ft_strdup(str[i]);
-	if (!map->no_path)
-		map_error(map, str, "allocating texture path");
+	i += (2 + skip_whitespaces(&str[i]));
+	if (indentifier == 'N')
+	{
+		map->no_path = ft_strdup(&str[i]);
+		if (!map->no_path)
+			map_error(map, str, "allocating texture path");
+	}
+	else if (indentifier == 'E')
+	{
+		map->ea_path = ft_strdup(&str[i]);
+		if (!map->ea_path)
+			map_error(map, str, "allocating texture path");
+	}
+	else if (indentifier == 'S')
+	{
+		map->so_path = ft_strdup(&str[i]);
+		if (!map->so_path)
+			map_error(map, str, "allocating texture path");
+	}
+	else if (indentifier == 'W')
+	{
+		map->we_path = ft_strdup(&str[i]);
+		if (!map->we_path)
+			map_error(map, str, "allocating texture path");
+	}
 }
 
 void	check_texture_path(t_map *map, char *str)
@@ -81,15 +89,15 @@ void	check_texture_path(t_map *map, char *str)
 	int	i;
 
 	i = 0;
-	i = skip_whitespaces(str[i]);
-	if (!ft_strncmp("NO", str[i], 2))
-		return (read_path(map, "NO", str, i));
-	else if (!ft_strncmp("SO", str[i], 2))
-		return (read_path(map, "SO", str, i));
-	else if (!ft_strncmp("WE", str[i], 2))
-		return (read_path(map, "WE", str, i));
-	else if (!ft_strncmp("EA", str[i], 2))
-		return (read_path(map, "EA", str, i));
+	i = skip_whitespaces(&str[i]);
+	if (!ft_strncmp("NO", &str[i], 2))
+		return (read_path(map, 'N', str, i));
+	else if (!ft_strncmp("SO", &str[i], 2))
+		return (read_path(map, 'S', str, i));
+	else if (!ft_strncmp("WE", &str[i], 2))
+		return (read_path(map, 'W', str, i));
+	else if (!ft_strncmp("EA", &str[i], 2))
+		return (read_path(map, 'E', str, i));
 	map_error(map, str, "no valid type identifier");
 }
 
@@ -103,7 +111,7 @@ int	handle_textures(t_map *map, int fd)
 		if (!str)
 			break ;
 		if (!ft_is_empty_line(str))
-			check_texture_path(map, str)
+			check_texture_path(map, str);
 		free(str);
 	}
 	return (0);
@@ -131,6 +139,5 @@ int main(int argc, char *argv[])
 	t_map	map;
 	(void)argc;
 	(void)argv;
-
-	handle_textures(&map, open("./map.cub", O_RDWR);
+	handle_textures(&map, open("./map.cub", O_RDWR));
 }
