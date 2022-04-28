c9caabc2750ee6b9a1c3caefdae965e67447beee
Ersin Ozben <eozben@student.42heilbronn.de>
2022-04-28 21:19:31 +0200

    compilation fixxed

--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parser.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fbindere <fbindere@student.42.fr>          +#+  +:+       +#+        */
+/*   By: eozben <eozben@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/15 22:47:18 by eozben            #+#    #+#             */
-/*   Updated: 2022/04/28 19:14:31 by fbindere         ###   ########.fr       */
+/*   Updated: 2022/04/28 21:16:59 by eozben           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ void	parse_cub_file(t_cub *cub, char **argv)
 	cub->map.fd = open(argv[1], O_RDWR);
 	if (cub->map.fd == ERROR)
 		map_error(cub, NULL, "Bad .cub img_arr");
-	read_map(cub);
 	read_textures(cub);
+	read_map(cub);
 	check_map_validity(cub, &cub->player);
 	close(cub->map.fd);
 }
